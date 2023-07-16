ab2cd5d334eabb7d46434b2abdbe4694640202bf
Joey <fangjin98@outlook.com>
2023-07-16 19:34:40 +0800

    organize data access

---
+++ src/Services/IDataAccess.cs
@@ -0,0 +1,21 @@
+﻿using DayDayUp.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DayDayUp.Services
+{
+    public interface IDataAccess
+    {
+        Task<List<Todo>> GetDataAsync();
+
+        List<Todo> GetData();
+
+        void AddDataAsync(Todo item);
+
+        Task DeleteDataAsync(Todo item);
+
+        Task UpdateDataAsync(Todo item);
+
+        void ExportToJson(string path);
+    }
+}
--- src/Services/LiteDbDataAccess.cs
+++ src/Services/LiteDbDataAccess.cs
@@ -9,21 +9,6 @@ using LiteDB;
 
 namespace DayDayUp.Services
 {
-    public interface IDataAccess
-    {
-        Task<List<Todo>> GetDataAsync();
-
-        List<Todo> GetData();
-
-        void AddDataAsync(Todo item);
-
-        Task DeleteDataAsync(Todo item);
-
-        Task UpdateDataAsync(Todo item);
-
-        void ExportToJson(string path);
-    }
-
     public class LiteDbDataAccess : IDataAccess
     {
         public LiteDbDataAccess()
